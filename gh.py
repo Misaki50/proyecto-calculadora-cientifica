b4f35ea0173bdd8438ddbb239780ea2dbd1a8297
Misaki50 <misakoremixes@gmail.com>
2026-06-22 13:06:33 -0500

    Update gh.py

--- gh.py
+++ gh.py
@@ -51,6 +51,7 @@ def borrar_uno():
 ventana = tk.Tk()
 ventana.title("Calculadora Científica")
 ventana.geometry("385x510")
+ventana.resizable(False, False)
 ventana.configure(bg="#1c1c1c")  # Fondo oscuro
 
 # Configuración de fuentes (Cámbialas aquí y se aplicarán a todo)
